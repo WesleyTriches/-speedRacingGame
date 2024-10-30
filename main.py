dfe60f81cb4d8c89fecc0b784d56e2d90b5e80b8
Wesley Triches <1137196@atitus.edu.br>
2024-10-29 21:05:08 -0300

    codInicial

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print('Hello World!')
-
-print(5+5)
+import pygame
+pygame.init()
+tamanho = (1000,592)
+clock = pygame.time.Clock()
+tela = pygame.display.set_mode( tamanho )
+pygame.display.set_caption('Corrida Maluca') #texto em cima 
+branco = (255,255,255)
+preto = (0,0,0)
+fundo = pygame.image.load('assets/fundo.png')
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            quit()
+            
+    tela.fill( branco )
+    tela.blit(fundo, (0,0)) #jogar a imagem do fundo para a tela
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+    
