734875e877068dce7ee5056823c3003afc1258bb
Wesley Triches <1137196@atitus.edu.br>
2024-10-29 21:10:38 -0300

    carrinhos adicionados

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ pygame.display.set_caption('Corrida Maluca') #texto em cima
 branco = (255,255,255)
 preto = (0,0,0)
 fundo = pygame.image.load('assets/fundo.png')
+carro1 = pygame.image.load('assets/carro1.png')
+carro2 = pygame.image.load('assets/carro2.png')
+#trilha = pygame
+#vitoria = pygame
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             quit()
             
-    tela.fill( branco )
+    tela.fill( branco ) #pinta de branco
     tela.blit(fundo, (0,0)) #jogar a imagem do fundo para a tela
+    tela.blit(carro1, (0,50))
+    tela.blit(carro2, (0,200))
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
