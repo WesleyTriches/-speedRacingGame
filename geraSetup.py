0738961681907e831e872ba134aa70b7abdb4be1
Estevan Fernandes <estevan.stolfo05@gmail.com>
2024-11-20 00:29:31 -0300

    Adição da terceira pista e criação do executavel

--- geraSetup.py
+++ geraSetup.py
@@ -2,14 +2,14 @@
 import cx_Freeze
 
 executables = [
-    cx_Freeze.Executable(script= 'main.py', icon='assets/icone.ico')
+    cx_Freeze.Executable(script= 'main.py', icon='recursos/icone.ico')
 ]
 cx_Freeze.setup(
     name = 'Corrida Maluca',
     options= {
         'build_exe':{
             'packages':['pygame'],
-            'include_files':['assets']
+            'include_files':['recursos']
         }
     }, executables = executables    
 )
